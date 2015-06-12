c8d9c1da1c29407a7d0577f2bf20ea59b54ddfb1
ronnybrunswijk <rienk.boonstra@gmail.com>
2015-06-12 12:44:57 +0000

    Removal gegenereerde testen

--- test/controllers/static_pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionController::TestCase
-  test "should get introductiepagina" do
-    get :introductiepagina
-    assert_response :success
-  end
-
-end
